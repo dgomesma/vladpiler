414d88ce872785f0c9152f618cab707c867e5c85
Daniel G. Maia Filho <dgomesma01@gmail.com>
2023-09-17 15:46:14 -0700

    Parially implement AST

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -43,12 +43,68 @@ void SymbolTableStack::popScope() {
   symbol_tables.pop_back();
 }
 
+namespace AST{
+  File::File(const std::string& _filename, std::unique_ptr<Term> _term)
+    : filename(_filename), term(std::move(_term)) {}
+
+  Int::Int(int64_t _value) : value(_value) {}
+
+  Str::Str(std::string&& _str) : str(std::move(_str)) {}
+  
+  Call::Call(Descriptor* _descriptor) {
+    assert(_descriptor);
+    descriptor = _descriptor;
+  };
+
+  Binary::Binary(std::unique_ptr<Term> _lhs, std::unique_ptr<Term> _rhs, BinOp _binop) :
+    lhs(std::move(_lhs)), rhs(std::move(_rhs)), binop(_binop) {}
+
+  Parameter::Parameter(std::string&& id, Descriptor* _descriptor) :
+    identifier(id), descriptor(_descriptor) {}
+
+  Parameters::Parameters(std::vector<Parameter>&& _params) : 
+    params(std::move(_params)) {} 
+
+  Parameters::Parameters(Parameters&& _params) :
+    params(std::move(_params.params)) {}
+
+  Function::Function(Parameters&& _parameters, std::unique_ptr<Term> _value) :
+    parameters{std::move(_parameters)}, value(std::move(_value)) {}
+
+  Let::Let(Parameter&& _parameter, std::unique_ptr<Term> _val, std::unique_ptr<Term> _next) :
+    parameter(std::move(_parameter)), val(std::move(_val)), next(std::move(_next)) {}
+
+  If::If(std::unique_ptr<Term> _condition, std::unique_ptr<Term> _then, std::unique_ptr<Term> _orElse) :
+    condition(std::move(_condition)), then(std::move(_then)), orElse(std::move(_orElse)) {}
+
+  Print::Print(std::unique_ptr<Term> _arg) :
+    arg(std::move(_arg)) {}
+
+  First::First(std::unique_ptr<Term> _arg) :
+    arg(std::move(_arg)) {}
+
+  Second::Second(std::unique_ptr<Term> _arg) :
+    arg(std::move(_arg)) {}
+
+  Tuple::Tuple(std::unique_ptr<Term> _first, std::unique_ptr<Term> _second) :
+    first(std::move(_first)), second(std::move(_second)) {}
+
+  Var::Var(std::string&& _name, Descriptor* _descriptor) :
+    name(std::move(_name)), descriptor(std::move(_descriptor)) {};
+}
+
 int Compiler::compile(const std::string& filename) {
   yyin = read_file(filename);
+  std::error_code fd_ostream_ec;
+  llvm::raw_fd_ostream ostream = llvm::raw_fd_ostream(filename, fd_ostream_ec);
+  llvm_module = new llvm::Module(filename, llvm_context);
+  
   int ret = yyparse();
   if (ret != 0) {
     std::cerr << "Error while parsing: " << strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
+
+  llvm_module->print(ostream, nullptr);
   return EXIT_SUCCESS;
 }
