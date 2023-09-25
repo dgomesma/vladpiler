873947f8d552e9b001958a0eca239a476e09fdbc
Daniel G. Maia Filho <dgomesma01@gmail.com>
2023-09-25 08:42:01 -0700

    Updates to rinha_extern

--- src/rinha_extern.c
+++ src/rinha_extern.c
@@ -1,49 +1,42 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void _print_undefined() {
+void print_undefined() {
   printf("undefined");
 }
 
-void print_undefined() {
-  _print_undefined();
-  printf("\n");
-}
-
-void _print_bool(uint8_t val) {
+void print_bool(uint8_t val) {
   if (val) printf("true");
   else printf("false");
 }
 
-void print_bool(uint8_t val) {
-  _print_bool(val);
-  printf("\n");
-}
-
-void _print_num(int32_t val) {
+void print_num(int32_t val) {
   printf("%d", val);
 }
 
-void print_num(int32_t val) {
-  _print_num(val);
-  printf("\n");
-}
 
-void _print_str(char* str) {
-  if (!str) _print_undefined();
+void print_str(char* str) {
+  if (!str) print_undefined();
   else printf("%s", str);
 }
 
-void print_str(char* str) {
-  _print_str(str);
-  printf("\n");
-}
 
-void _print_closure(void) {
+void print_closure(void) {
   printf("<#closure>");
 }
 
-void print_closure(void) {
-  _print_closure();
+void print_lp(void) {
+  printf("(");
+}
+
+void print_delim(void) {
+  printf(", ");
+}
+
+void print_rp(void) {
+  printf(")");
+}
+
+void print_nl(void) {
   printf("\n");
 }
