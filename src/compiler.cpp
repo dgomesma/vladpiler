ffcbe0e5f961167da1c8f487f2f7a6c17e5b5dbb
Daniel G. Maia Filho <dgomesma01@gmail.com>
2023-09-27 04:24:18 -0700

    Fundamentally change symbol table

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -141,98 +141,26 @@ namespace AST{
 
 namespace Compiler {
 
-  SymbolTableStack::Identifier::Identifier(const std::string& _name):
-    isFn(false),
-    name(_name) {}
-  
-  SymbolTableStack::Identifier::Identifier(const std::string& _name, llvm::Type* _ret, std::initializer_list<llvm::Type*> _args) :
-    isFn(true),
-    name(_name),
-    ret(_ret),
-    args(_args) {}
-
-  SymbolTableStack::Identifier::Identifier(const std::string& _name, llvm::Type* _ret, std::vector<llvm::Type*> _args) :
-    isFn(true),
-    name(_name),
-    ret(_ret),
-    args(_args) {}
-     
   SymbolTableStack::SymbolTableStack() {
     pushScope();  // Global Data
   }
 
-  bool SymbolTableStack::Identifier::operator==(const Identifier& other) const {
-    if (name != other.name) return false;
-    if (!isFn && !other.isFn) return true;
-    else if (isFn == other.isFn) {
-      if (ret != other.ret) return false;
-      if (args.size() != other.args.size()) return false;
-      for (uint32_t i = 0; i < args.size(); i++) 
-        if (args[i] != other.args[i]) return false;
-      return true;
-    } else return false;
-  }
-
-  std::size_t SymbolTableStack::IdentifierHasher::operator()(const Identifier& id) const {
-    std::size_t hash = std::hash<std::string>()(id.name);
-    if (!id.isFn) return hash;
-    hash ^= std::hash<llvm::Type*>()(id.ret);
-    for (llvm::Type* arg : id.args) {
-      hash ^= std::hash<llvm::Type*>()(arg);
-    }
-    return hash;
- }
-
-  llvm::Value* SymbolTableStack::getValue(Identifier id) const {
-    for (auto it = symbol_tables.crbegin(); it != symbol_tables.crend(); it++) {
-      SymbolTable::const_iterator descriptor = it->find(id);
-      if (descriptor != it->end()) return descriptor->second;
+  EitherValOrClosure* SymbolTableStack::getValue(const std::string& id) {
+    for (auto it = symbol_tables.rbegin(); it != symbol_tables.rend(); it++) {
+      SymbolTable::iterator descriptor = it->find(id);
+      if (descriptor != it->end()) return &descriptor->second;
     }
     return nullptr;
   }
 
-  llvm::Value* SymbolTableStack::getVariable(const std::string& name) const {
-    Identifier id(name);
-    return getValue(id);
-  }
-  
-  llvm::Function* SymbolTableStack::getFunction(const std::string& name, llvm::Type* ret, const std::vector<llvm::Type*>& args) const {
-    Identifier id(name, ret, args);
-    return reinterpret_cast<llvm::Function*>(getValue(id));
+  void SymbolTableStack::insertValue(const std::string& name, llvm::Value* val) {
+    symbol_tables.back()[name] = {val, nullptr};
   }
 
-  void SymbolTableStack::insertValue(const Identifier& id, llvm::Value* value) {
-    assert(value);
-    SymbolTable& cur_scope = symbol_tables.back();
-    cur_scope[id] = value;
-  };
+  void SymbolTableStack::insertClosure(const std::string& name, const ClosureSignature& closure_sig) {
+    symbol_tables.back()[name] = {nullptr, std::make_unique<ClosureSignature>(closure_sig)};
+  }
  
-  void SymbolTableStack::insertVariable(const std::string& name, llvm::Value* value) {
-    Identifier id(name);
-    insertValue(id, value);
-  }
-
-  void SymbolTableStack::insertFunction(const std::string& name, llvm::Type* ret, const std::vector<llvm::Type*>& args, llvm::Function* value) {
-    Identifier id(name, ret, args);
-    insertValue(id, value);
-  };
-
-   void SymbolTableStack::insertGlobalValue(const Identifier& id, llvm::Value* value) {
-    assert(value);
-    SymbolTable& cur_scope = symbol_tables.front();
-    cur_scope[id] = value;
-  };
- 
-  void SymbolTableStack::insertGlobalVariable(const std::string& name, llvm::Value* value) {
-    Identifier id(name);
-    insertGlobalValue(id, value);
-  }
-
-  void SymbolTableStack::insertGlobalFunction(const std::string& name, llvm::Type* ret, const std::vector<llvm::Type*>& args, llvm::Function* value) {
-    Identifier id(name, ret, args);
-    insertGlobalValue(id, value);
-  }; 
-
   void SymbolTableStack::pushScope() {
     symbol_tables.emplace_back();
   }
@@ -291,41 +219,61 @@ namespace Compiler {
     return llvm::FunctionType::get(default_type, args, false);
   }
 
-  llvm::Function* RinhaCompiler::createClosure(const std::string& name, uint32_t n_args) {
-    llvm::FunctionType* default_fn_type = getDefaultFnType(n_args);
-    llvm::Function* closure = llvm::Function::Create(default_fn_type, llvm::Function::ExternalLinkage, name, module);
-    symtbl_stack.insertFunction(name, default_type, default_fn_type->params(), closure);
-    return closure;
+
+  llvm::Value* RinhaCompiler::createClosure(const std::string& name, uint32_t n_args, AST::Term* fn_body) {
+    closure_table[name] = {name, n_args, fn_body};
+    return createClosureVal();
   }
 
-  llvm::Function* RinhaCompiler::createFunction(llvm::Type* ret, const std::vector<llvm::Type*>& args, const std::string& name) {
-    llvm::FunctionType* fn_type = llvm::FunctionType::get(ret, args, false);
+  llvm::Value* RinhaCompiler::callClosure(const std::string& name, std::vector<llvm::Value*> args) {
+    // TODO: Finish
+    auto opt_closure_sig = closure_table.find(name);
+    if (opt_closure_sig == closure_table.end()) {
+      std::cerr << "Warning: Trying to call undefined function " + name << std::endl;;
+      return createUndefined();
+    }
+
+    const ClosureSignature& closure_sig = opt_closure_sig->second;
+    if (args.size() != closure_sig.n_args) {
+      std::cerr << "On " + name + " function call: number of arguments don't match" << std::endl;
+      return createUndefined();
+    }
+
+    std::vector<llvm::Type*> arg_types;
+    for (llvm::Value* arg : args) arg_types.push_back(arg->getType()); 
+   
+    llvm::FunctionType* fn_type = llvm::FunctionType::get(llvm::Type::getVoidTy(context), arg_types, false);    
     llvm::Function* fn = llvm::Function::Create(fn_type, llvm::Function::ExternalLinkage, name, module);
-    
+    llvm::BasicBlock* fn_entry = llvm::BasicBlock::Create(context, "entry", fn);
+    builder.SetInsertPoint(fn_entry);
     symtbl_stack.pushScope();
-        
-    return fn;
+    
+    return nullptr;        
   }
-
-  llvm::Function* RinhaCompiler::createFunction(llvm::Type* type, std::initializer_list<llvm::Type*> _args, const std::string& name) {
-    const std::vector<llvm::Type*> args(_args);
-    return createFunction(type, args, name);
-  };  
 
   llvm::Value* RinhaCompiler::getVariable(const std::string& name) {
-    llvm::Value* var = symtbl_stack.getVariable(name);
-    return var ?  var : createUndefined();
+    EitherValOrClosure* var = symtbl_stack.getValue(name);
+    if (!var) {
+      std::cerr << "Warning: reference to undefined variable " << name << std::endl;
+      return createUndefined();
+    }
+
+    if (!var->val) {
+      std::cerr << "Warning: "<< name << " is a closure" << std::endl;
+    }
+
+    return var->val;
   }
 
   llvm::Function* RinhaCompiler::getExternFunction(llvm::Type* ret, const std::vector<llvm::Type*>& args, const std::string& name) {
-    llvm::Function* print_fn = symtbl_stack.getFunction(name, ret, args);    
-    if (print_fn) return print_fn;
+    llvm::Function* fn = extern_fn_table[name];
+    if (fn) return fn;
 
     llvm::IRBuilder<>::InsertPoint previous_point= builder.saveIP();
     builder.restoreIP(externInsertPoint);
     llvm::FunctionType* fn_type = llvm::FunctionType::get(ret, args, false);
     llvm::Function* extern_fn = llvm::Function::Create(fn_type, llvm::Function::ExternalLinkage, name, module);
-    symtbl_stack.insertGlobalFunction(name, ret, args, extern_fn);
+    extern_fn_table[name] = fn;
     
     builder.restoreIP(previous_point);
     return extern_fn;
@@ -540,7 +488,16 @@ namespace Compiler {
 
   llvm::Value* RinhaCompiler::createUndefined() {
     llvm::Value* zero = builder.getInt8(0);
-    return builder.CreateIntToPtr(zero, llvm::Type::getInt8PtrTy(context));
+    llvm::Value* undef = builder.CreateIntToPtr(zero, llvm::Type::getInt8PtrTy(context));
+    special_value_table[undef] = SpecialValue::UNDEFINED;
+    return undef;
+  }
+  
+  llvm::Value* RinhaCompiler::createClosureVal() {
+    llvm::Value* zero = builder.getInt8(0);
+    llvm::Value* undef = builder.CreateIntToPtr(zero, llvm::Type::getInt8PtrTy(context));
+    special_value_table[undef] = SpecialValue::CLOSURE;
+    return undef;
   }
 
   void RinhaCompiler::createVoidReturn() {
@@ -593,7 +550,7 @@ namespace Compiler {
    }
 
   void RinhaCompiler::createVariable(const std::string& identifier, llvm::Value* val) {
-    symtbl_stack.insertVariable(identifier, val);
+    symtbl_stack.insertValue(identifier, val);
   }
 
   llvm::Type* RinhaCompiler::getPtrType(llvm::Value* val) {
@@ -695,8 +652,8 @@ namespace Compiler {
   }
 
   void RinhaCompiler::_print(llvm::Value* val) {
-    std::string print_name = "print_undefined";
-    std::vector<llvm::Type*> args = {};
+    std::string print_name;
+    std::vector<llvm::Type*> args;
     llvm::Type* type = val->getType();
 
     if (type->isIntegerTy(1)) {
@@ -710,32 +667,35 @@ namespace Compiler {
 
     } else if (type->isPointerTy()) {
       llvm::Type* ptr_type = ptr_type_table[ptr_id_table[val]];
-      assert(ptr_type);
+      if (!ptr_type) {
+      const SpecialValue special_value = special_value_table[val];
+        
+        if (special_value == SpecialValue::UNDEFINED) {
+          print_name = "print_undefined";
+          args = {};
+        }
 
-      
-      if (ptr_type->isArrayTy() && ptr_type->getArrayElementType()->isIntegerTy(8)) {
-        print_name = "print_str";
-        args = {llvm::Type::getInt8PtrTy(context)};
-      }
+        else if (special_value_table[val] == SpecialValue::CLOSURE) {
+          print_name = "print_closure";
+          args = {};
+        }
 
-      else if (ptr_type->isStructTy()) {
-        printTuple(val);  
-        return;
-      }
-      /*
-      ptr_type_table[val->getName().str()] << std::endl;
-      llvm::Type* ptr_type = getPtrType(val);
-      if (ptr_type->isStructTy()) {
-        printTuple(val);
-        return;
-      } else if (ptr_type->isArrayTy() && ptr_type->getArrayElementType()->isIntegerTy(8)) {
-        print_name = "print_str";
-        args = {llvm::Type::getInt8PtrTy(context)};
-      } else if (ptr_type->isPointerTy()) {
+        else {
+          throw std::runtime_error("Error: could not find the ID for the pointer.");
+        }
 
-       
+      } else {
+     
+        if (ptr_type->isArrayTy() && ptr_type->getArrayElementType()->isIntegerTy(8)) {
+          print_name = "print_str";
+          args = {llvm::Type::getInt8PtrTy(context)};
+        }
+
+        else if (ptr_type->isStructTy()) {
+          printTuple(val);  
+          return;
+        }
       }
-    */
     } else if (type->isFunctionTy()) {
       print_name = "print_closure";
     } else {
